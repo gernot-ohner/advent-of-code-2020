2be032b212581e51494d6a2dd07ed9e8dc69e895
Gernot Ohner <gernot.ohner@gmail.com>
2020-12-02 22:36:46 +0100

    [DAY-2] even more idiomatic version

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -4,17 +4,17 @@ object day2 extends App {
 
   val passwordEntryPattern = "(\\d+)-(\\d+) (\\w): (\\w+)".r
 
-  val oldCount = data.map(passwordEntry => {
+  val oldCount = data.count(passwordEntry => {
     val passwordEntryPattern(lower, upper, char, password) = passwordEntry
     val count: Int = password.count(char == _.toString)
     (count >= lower.toInt) && (count <= upper.toInt)
-  }).count(_ == true)
+  })
 
-  val newCount = data.map(passwordEntry => {
+  val newCount = data.count(passwordEntry => {
     val passwordEntryPattern(lower, upper, char, password) = passwordEntry
     (password(lower.toInt - 1).toString == char) ^
       (password(upper.toInt - 1).toString == char)
-  }).count(_ == true)
+  })
 
   println(s"Correct passwords according to old policy: $oldCount")
   println(s"Correct passwords according to new policy: $newCount")
