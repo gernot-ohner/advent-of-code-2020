11a18b3ad1536d8d708b474e961313c416a2e398
Gernot Ohner <gernot.ohner@gmail.com>
2020-12-02 22:25:55 +0100

    [DAY-2] more idiomatic version

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -1,41 +1,21 @@
-import scala.util.matching.Regex
-
 object day2 extends App {
 
-  private val passwordRegex = new Regex("(\\d+)-(\\d+) (\\w): (\\w+)", "lower", "upper", "char", "password")
-  private val data: List[String] = Util.getData("day2")
+  val data: List[String] = Util.getData("day2")
 
-  def parsePasswordEntry(string: String): PasswordEntry = {
+  val passwordEntryPattern = "(\\d+)-(\\d+) (\\w): (\\w+)".r
 
-    val matchData: Regex.Match = passwordRegex.findFirstMatchIn(string).get
-    new PasswordEntry(
-      matchData.group("lower").toInt,
-      matchData.group("upper").toInt,
-      matchData.group("char"),
-      matchData.group("password")
-    )
-  }
+  val oldCount = data.map(passwordEntry => {
+    val passwordEntryPattern(lower, upper, char, password) = passwordEntry
+    val count: Int = password.count(char == _.toString)
+    (count >= lower.toInt) && (count <= upper.toInt)
+  }).count(_ == true)
 
-  private val passwordEntries: List[PasswordEntry] = data.map(parsePasswordEntry)
+  val newCount = data.map(passwordEntry => {
+    val passwordEntryPattern(lower, upper, char, password) = passwordEntry
+    (password(lower.toInt - 1).toString == char) ^
+      (password(upper.toInt - 1).toString == char)
+  }).count(_ == true)
 
-  def validateOldPassword(passwordEntry: PasswordEntry): Boolean = {
-    val count = passwordEntry.password.count(passwordEntry.char == _.toString)
-    count >= passwordEntry.lower && count <= passwordEntry.upper
-  }
-
-  def validateNewPassword(passwordEntry: PasswordEntry): Boolean = {
-    (passwordEntry.password(passwordEntry.lower - 1).toString == passwordEntry.char) ^
-      (passwordEntry.password(passwordEntry.upper - 1).toString == passwordEntry.char)
-  }
-
-
-  val oldCount = passwordEntries.map(validateOldPassword).count(_ == true)
-  val newCount = passwordEntries.map(validateNewPassword).count(_ == true)
-  println(newCount)
-
-
-
-
+  println(s"Correct passwords according to old policy: $oldCount")
+  println(s"Correct passwords according to new policy: $newCount")
 }
-
-class PasswordEntry(val lower: Int, val upper: Int, val char: String, val password: String)
