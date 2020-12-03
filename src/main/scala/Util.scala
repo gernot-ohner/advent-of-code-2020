1236ed2b9b22d008868fe557deeb51ddb0a03b92
Gernot Ohner <gernot.ohner@gmail.com>
2020-12-03 07:48:14 +0100

    [DAY-2] makes input files .txt

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -3,7 +3,7 @@ import scala.io.{BufferedSource, Source}
 object Util {
 
   def getData(day: String): List[String] = {
-    val filepath = s"inputs/$day"
+    val filepath = s"inputs/$day.txt"
     val file: BufferedSource = Source.fromResource(filepath)
     val data = file.getLines().toList
     file.close
