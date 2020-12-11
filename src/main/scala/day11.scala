83a2ac302c1c5c449eda12f93ff4660a2e67b734
Gernot Ohner <gernot.ohner@gmail.com>
2020-12-11 22:08:49 +0100

    [DAY-11] part 2

--- src/main/scala/day11.scala
+++ src/main/scala/day11.scala
@@ -3,33 +3,45 @@ object day11 extends App {
   val availableSeat = "L"
   val occupiedSeat = "#"
   val data: Array[Array[String]] = Util.getDataInLines("day11").map(_.split("")).toArray
-  val seatMap = data.map(_.map(s => s == availableSeat))
 
-  def surroundingSeats(x: Int, y: Int) = {
-    val allSurroundingPositions = List(
-      (x + 1, y + 1), (x + 1, y), (x + 1, y - 1),
-      (x, y + 1), (x, y - 1),
-      (x - 1, y + 1), (x - 1, y), (x - 1, y - 1)
-    )
-    allSurroundingPositions.filter(position =>
-      data.indices.contains(position._1)
-        && data.head.indices.contains(position._2)
-        && seatMap(position._1)(position._2))
+  def visibleOccupiedSeats(state: Array[Array[String]], x: Int, y: Int) = {
+    val xPlus = (x until data.length).tail
+    val xMinus = (x to 0 by -1).tail
+    val yPlus = (y until data.head.length).tail
+    val yMinus = (y to 0 by -1).tail
+
+    val right = xPlus.map((_, y))
+    val left = xMinus.map((_, y))
+    val up = yPlus.map((x, _))
+    val down = yMinus.map((x, _))
+    val rightUp = xPlus.zip(yPlus)
+    val rightDown = xPlus.zip(yMinus)
+    val leftUp = xMinus.zip(yPlus)
+    val leftDown = xMinus.zip(yMinus)
+
+    def findVisibleOccupiedSeat(range: Seq[(Int, Int)]) = {
+      range.takeWhile(pos => state(pos._1)(pos._2) != availableSeat)
+        .find(pos => state(pos._1)(pos._2) == occupiedSeat)
+    }
+
+    List(right, left, up, down, rightUp, rightDown, leftUp, leftDown)
+      .map(findVisibleOccupiedSeat)
+      .count(_.isDefined)
   }
 
-  def computeOccupancy(data: Array[Array[String]], x: Int, y: Int) = {
-    val occupiedSeatsNearby = surroundingSeats(x, y).map(position => data(position._1)(position._2)).count(_ == occupiedSeat)
-    val seat = data(x)(y)
-    if (seat == availableSeat && occupiedSeatsNearby == 0) {
+  def computeOccupancy(state: Array[Array[String]], x: Int, y: Int) = {
+    val numOfVisibleOccupiedSeats = visibleOccupiedSeats(state, x, y)
+    val seat = state(x)(y)
+    if (seat == availableSeat && numOfVisibleOccupiedSeats == 0) {
       occupiedSeat
-    } else if (seat == occupiedSeat && occupiedSeatsNearby >= 4) {
+    } else if (seat == occupiedSeat && numOfVisibleOccupiedSeats >= 5) {
       availableSeat
     } else {
       seat
     }
   }
 
-  def deepEquals (a1: Array[Array[String]], a2: Array[Array[String]]) = {
+  def deepEquals(a1: Array[Array[String]], a2: Array[Array[String]]) = {
     val flatten = a2.flatten
     a1.flatten.sameElements(flatten)
   }
@@ -45,14 +57,11 @@ object day11 extends App {
   }
 
   def countOccupied(state: Array[Array[String]]): Int = {
-    (for {
-      x <- state
-      y <- x
-    } yield y == occupiedSeat).count(_ == true)
+    (for {x <- state; y <- x} yield y == occupiedSeat).count(_ == true)
   }
 
   private val finalState: Array[Array[String]] = update(data)
   private val occupiedSeatsInFinalState: Int = countOccupied(finalState)
-  println(s"In the final state $occupiedSeatsInFinalState are occupied")
+  println(s"In the final state $occupiedSeatsInFinalState seats are occupied")
 
 }
